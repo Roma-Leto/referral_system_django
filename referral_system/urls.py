81b01c1807a9756f88e5dea9eb47c7408295eb27
Roma-Leto <imletoroma@gmail.com>
2024-11-30 23:58:07 +0300

    Описание маршрутов.

--- referral_system/urls.py
+++ referral_system/urls.py
@@ -1,22 +1,30 @@
-"""
-URL configuration for referral_system project.
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from users import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Referral System API",
+        default_version='v1',
+        description="API простой реферальной системы",
+        contact=openapi.Contact(email="imletoroma@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],  # Уровень доступа
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('phone/', views.PhoneNumberView.as_view(), name='phone_number'),
+    path('verify/', views.VerificationCodeView.as_view(), name='verify_code'),
+    path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('activate_invite_code/', views.ActivateInviteCodeView.as_view(),
+         name='activate_invite_code'),
+    path('api-auth/', include('rest_framework.urls')),
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
